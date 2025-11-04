0c1fa21e583735b9e584c0f5986216964f3c8bc8
lukaszjanus03 <lukaszjanus@microsoft.wsei.edu.pl>
2025-11-04 14:04:39 +0100

    lab05

--- src/components/FooterApp.jsx
+++ src/components/FooterApp.jsx
@@ -2,10 +2,10 @@ function FooterApp() {
   return (
     <footer className="mt-4 p-3 bg-light text-center border-top">
       <img
-        src="/logo.svg"
-        alt="Logo uczelni"
-        style={{ height: "30px", marginRight: "10px" }}
-      />
+    src="https://d1ll4kxfi4ofbm.cloudfront.net/file/user/organizer/285039/logo1.png" // <-- Pełny adres URL
+    alt="Logo uczelni"
+    style={{ height: "30px", marginRight: "10px" }}
+/>
       © 2025 Łukasz Janus 
     </footer>
   );
